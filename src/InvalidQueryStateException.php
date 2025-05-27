7e9d19cd0592c9a6cb4eb5130d022c4923642469
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2025-05-26 23:56:06 -0400

    fix: remove redundant namespace sub-level
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- src/InvalidQueryStateException.php
+++ src/InvalidQueryStateException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Phpolar\Phpolar\Storage;
+namespace Phpolar\Storage;
 
 use RuntimeException;
 
--- src/KeyNotFound.php
+++ src/KeyNotFound.php
@@ -2,11 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Phpolar\Phpolar\Storage;
+namespace Phpolar\Storage;
 
 /**
  * Represents the scenario when the key of an item is not found.
  */
-final class KeyNotFound
-{
-}
+final class KeyNotFound {}
